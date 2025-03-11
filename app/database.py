43d335885d0bcae96a9577a6b75d8dc46b2535bb
Dalai Aguirre <dalai2123@hotmail.com>
2025-03-10 18:50:59 -0600

    Environment variable for db connection and rollback for docker-compose file

--- app/database.py
+++ app/database.py
@@ -1,9 +1,14 @@
-from sqlalchemy.orm import Session
-from models import SessionLocal
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
 
-# Dependency to get the database session
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres_user:postgres_password@db:5432/postgres_db")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 def get_db():
-    db: Session = SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
